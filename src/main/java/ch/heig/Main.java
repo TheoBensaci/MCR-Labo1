264cd1cd5ec240c06076e1d0b968f8940bb43b26
Maxime Regenass <maxime.regenass@hes-so.ch>
2026-02-26 15:01:53 +0100

    refactor Main

--- src/main/java/ch/heig/Main.java
+++ src/main/java/ch/heig/Main.java
@@ -9,25 +9,23 @@ import ch.heig.utils.Vector2f;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 public class Main {
 
     public final static int UPDATE_RATE=10;
     public final static int WIDTH=800;
     public final static int HEIGHT=800;
-
     public final static int MIN_SIZE=20;
-
     public final static int MAX_SIZE=50-MIN_SIZE;
 
+    private final LinkedList<Shape> bouncers = new LinkedList<>();
+    private Window win;
 
-    public static void main(String[] args) {
-        Window win = Window.getInstance();
+
+    public Main(){
+        win = Window.getInstance();
         win.setTitle("Bouncer");
-
-        List<Shape> listShape = new ArrayList<>();
 
 
         // init shapes
@@ -35,7 +33,7 @@ public class Main {
         for (int i = 0; i < nbCercle; ++i){
             Circle c = new Circle(MIN_SIZE+(int)(Math.random() * MAX_SIZE),(int)(Math.random()*WIDTH),(int)(Math.random()*HEIGHT),new Vector2f((float)Math.random(),(float) Math.random()).normilize().mult((float)Math.random()*10));
             win.getCanvas().addShape(c.getRenderShape());
-            listShape.add(c);
+            bouncers.add(c);
         }
 
         int nbSquare = (int)(Math.random() * 50);
@@ -43,16 +41,16 @@ public class Main {
             int size =MIN_SIZE+(int)(Math.random() * MAX_SIZE);
             Square s = new Square(size,size,(int)(Math.random()*WIDTH),(int)(Math.random()*HEIGHT),new Vector2f((float)Math.random(),(float) Math.random()).normilize().mult((float)Math.random()*10));
             win.getCanvas().addShape(s.getRenderShape());
-            listShape.add(s);
+            bouncers.add(s);
         }
+    }
 
-
-
+    public void run(){
         Timer timer = new Timer(UPDATE_RATE, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
 
-                for (Shape s : listShape){
+                for (Shape s : bouncers){
                     s.update();
                 }
 
@@ -64,5 +62,10 @@ public class Main {
         timer.start(); // Go go go!
 
         win.setUpdateTimer(timer);
+    }
+
+
+    public static void main(String[] args) {
+        new Main().run();
     }
 }
